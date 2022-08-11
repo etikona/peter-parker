2b4b3fd34491b5e3b50c3798feb66ba9f670c958
eti kona <etikonapal@gmail.com>
2022-08-12 00:52:11 +0600

    all done

---
+++ practice.js
@@ -0,0 +1,16 @@
+function compare(a, b) {
+    if (a.toString() === b) {
+     return true;
+    } else {
+     return false;
+    }
+   }
+   const result = compare(25, 25);
+//    console.log(result);
+let a= "hi"
+let b="there"
+// console.log(a+b)
+
+for(let i =5; i <= 100; i++){
+    console.log(i);
+}
